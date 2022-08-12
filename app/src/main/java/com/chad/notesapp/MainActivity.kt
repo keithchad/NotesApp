7b44092f28e8a487660e1186bd4e7655d895f4e1
keithchad <keithchad10@outlook.com>
2022-08-12 11:34:09 +0300

    implementing splash screen

--- app/src/main/java/com/chad/notesapp/MainActivity.kt
+++ app/src/main/java/com/chad/notesapp/MainActivity.kt
@@ -10,20 +10,16 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.chad.notesapp.ui.theme.NotesAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             NotesAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
